02e14c69e071adcf4aaf0639bc743c8c53a78c2f
onion0904 <takerudaze0904@gmain.com>
2024-08-09 00:20:54 +0900

    update TODOlist

--- main.go
+++ main.go
@@ -5,23 +5,24 @@ import(
 	"github.com/gin-gonic/gin"
     "gorm.io/driver/mysql"
     "gorm.io/gorm"
+	"os"
 )
 
-type User struct {
-    gorm.Model
-    Name  string
-    Email string
-    Age   int
-}
+// type User struct {
+//     gorm.Model
+//     Name  string
+//     Email string
+//     Age   int
+// }
 
-
-//テスト
-func main() {
-
-    dbUser := "onion"
-	dbPassword := "noino"
-	dbDatabase := "Todo_db"
+var (
+	dbUser := os.Getenv("USERNAME")
+	dbPassword := os.Getenv("USERPASS")
+	dbDatabase := os.Getenv("DATABASE")
 	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3307)/%s?parseTime=true", dbUser,dbPassword, dbDatabase)
+)
+
+func main() {
     db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
     if err != nil {
         panic("failed to connect database")
@@ -29,24 +30,34 @@ func main() {
 	db.AutoMigrate(&User{})
 	db.Create(&User{Name: "John Doe", Email: "john@example.com", Age: 25})
 
-	r := gin.Default()
+	Todo := gin.Default()
 
-    r.GET("/user/:id", func(c *gin.Context) {
-        var user User
-        id := c.Param("id")
+	method := Todo.Group("Todo"){
+		method.POST("/add",controller.Add)
+		method.GET("/list",controller.List)
+		method.PUT("/update",controller.Update)
+		method.DELETE("/delete",controller.Delete)
+	}
+
+
+	//test
+
+    // r.GET("/user/:id", func(c *gin.Context) {
+    //     var user User
+    //     id := c.Param("id")
         
-        result := db.First(&user, id)
-        if result.Error != nil {
-            if result.Error == gorm.ErrRecordNotFound {
-                c.JSON(404, gin.H{"error": "User not found"})
-                return
-            }
-            c.JSON(500, gin.H{"error": result.Error.Error()})
-            return
-        }
+    //     result := db.First(&user, id)
+    //     if result.Error != nil {
+    //         if result.Error == gorm.ErrRecordNotFound {
+    //             c.JSON(404, gin.H{"error": "User not found"})
+    //             return
+    //         }
+    //         c.JSON(500, gin.H{"error": result.Error.Error()})
+    //         return
+    //     }
 
-        c.JSON(200, user)
-    })
+    //     c.JSON(200, user)
+    // })
 
-    r.Run()
+    r.Run(":8080")
 }
